d43a7bc5cc9fece7c6df5e73085a8c68ae67d49e
Aleksandr Artemenkov <alartum@gmail.com>
2017-02-15 19:18:12 +0300

    Added hash

--- storage.h
+++ storage.h
@@ -8,12 +8,16 @@ An interface for different approaches of data storage (hash table, black-red tre
 typedef struct storage_t{
     void* manager; /**< The data structure responsible for storing and operating the data. */
 
-    void (*add)(void* self, const void* element);
-    void (*drop)(void* self, const void* element);
-    void* (*find)(const void* self, const void* element);
-    void (*delete)(void* self);
+    size_t size;
+    
+    int   (*add)  (void* self, const void* element);
+    void  (*drop) (void* self, const void* element);
+    void* (*find) (const void* self, const void* element);
+    int   (*clear)(void* self, const void* element)
+    int   (*delete)(void* self);
 } storage_t;
 
-storage_t* RBTree_ctor();
+storage_t* RBTree_ctor(size_t element_size);
+storage_t* init_hash();
 
 #endif // STORAGE_H
