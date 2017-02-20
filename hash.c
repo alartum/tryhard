51a704359eb28fda86478c3758c4c70dccbaeef5
Andrey Grabovoy <andriy.graboviy@mail.ru>
2017-02-20 19:39:54 +0300

    Change struct from set to map. Hide "maneger" from user. Fixed some bugs in hash.

--- hash.c
+++ hash.c
@@ -2,130 +2,195 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <limits.h>
-#define BITS_IN_int     ( sizeof(int) * CHAR_BIT )
-#define THREE_QUARTERS  ((int) ((BITS_IN_int * 3) / 4))
-#define ONE_EIGHTH      ((int) (BITS_IN_int / 8))
-#define HIGH_BITS       ( ~((unsigned int)(~0) >> ONE_EIGHTH ))
 
-struct sequence{
-	int (*insert)();
-	int (*delete)();
-	void* (*find)();
-	void* who; 
-} typedef sequence;
+#define BITS_IN_int (sizeof(int) * CHAR_BIT)
+#define THREE_QUARTERS  ((int) ((BITS_IN_int * 3) / 4))
+#define ONE_EIGHTH ((int) (BITS_IN_int / 8))
+#define HIGH_BITS  (~((size_t)(~0) >> ONE_EIGHTH))
+
+typedef struct storage_t{
+	int (*add)(void* self, const void* key, const void* element);
+	void (*drop)(void* self, const void* key);
+	void* (*find)(const void* self, const void* key);
+	int (*clear)(void* self);
+	int (*delete)(void* self);
+	void* manager;
+	size_t size;
+} storage_t;
 
 struct list typedef list;
 
 struct list{
 	list* next;
-	void* data;
+	void* element;
 	void* key;
 } typedef list;
 
 struct hash{
 	size_t blsize;
 	size_t tbsize;
+	size_t (*HashFunc)();
 	list* table;
 } typedef hash;
 
-extern unsigned int HashFunc(const char* data, unsigned int* key_size){
-	unsigned int hash_value, i;
-	*key_size = 0;
-	for(hash_value = 0; *data; ++data){
-		hash_value = ( hash_value << ONE_EIGHTH ) + *data;
+size_t HashAllData(const char* element, size_t* key_size, size_t size){
+	size_t hash_value, i;
+	for(hash_value = 0; *element; ++element){
+		hash_value = ( hash_value << ONE_EIGHTH ) + *element;
 		if (( i = hash_value & HIGH_BITS ) != 0 )
 			hash_value = (hash_value ^ (i >> THREE_QUARTERS)) & ~HIGH_BITS;
-		hash_value %= 1024;
+		hash_value %= size;
 		*key_size++;
 	}
 	return hash_value;
 }
 
-extern int insert_hash(void* key, void* data, sequence* _var){
-	unsigned int key_size;
-	unsigned int tg = HashFunc(key, &key_size);
+int add_hash  (void* self, const void* key, const void* element){
+	size_t key_size = 1;
+	size_t tg = (((hash*)(((storage_t*)self)->manager))->HashFunc)(key, &key_size, ((storage_t*)self)->size);
 	key_size++;
-	hash* var = _var->who;
+	hash* var = ((storage_t*)self)->manager;
 	list* lst = &(var->table[tg]);
 
 	while(lst->next != NULL){
-		if(!strncmp((char*)key, (char*)lst->key, key_size)){
+		if(!strncmp((const char*)key, (const char*)lst->key, key_size)){
 			return 1;		
 		}
 		lst = lst->next;
 	}
 
-	lst->data = malloc(var->blsize * sizeof(char));
-	lst->key = malloc(key_size * sizeof(char));
-	strncpy(lst->data, (char*)data, var->blsize);
-	strncpy(lst->key, (char*)key, key_size);
-	lst->next = malloc(sizeof(list));
-
+	lst->element = (void*)malloc(var->blsize * sizeof(char));
+	lst->key = (void*)malloc(key_size * sizeof(char));
+	strncpy((char*)lst->element, (const char*)element, var->blsize);
+	strncpy((char*)lst->key, (const char*)key, key_size);
+	lst->next = (list*)malloc(sizeof(list));
+	lst->next->next = NULL;
+	lst->next->element = NULL;
+	lst->next->key = NULL;
 	return 0;
 }
 
-extern int delete_hash(void* key, sequence* _var){
-	unsigned int key_size;
-	unsigned int tg = HashFunc(key, &key_size);
+void drop_hash(void* self, const void* key){
+	size_t key_size = 1;
+	size_t tg = (((hash*)(((storage_t*)self)->manager))->HashFunc)(key, &key_size, ((storage_t*)self)->size);
 	key_size++;
-	hash* var = _var->who;
+	hash* var = ((storage_t*)self)->manager;
 
 	list* lst = &(var->table[tg]);
 
 	list* prev = NULL;
 
 	while(lst->next != NULL){
-		if(!strncmp((char*)key, (char*)lst->key, key_size)){
+		if(!strncmp((const char*)key, (const char*)lst->key, key_size)){
 			if(prev != NULL){
 				prev->next = lst->next;
 				if(lst->key)
 					free(lst->key);
-				if(lst->data)
-					free(lst->data);
-				free(lst);
+				if(lst->element)
+					free(lst->element);
+				if(lst)
+					free(lst);
 			}else{
 				if(lst->key)
 					free(lst->key);
-				if(lst->data)
-					free(lst->data);
+				if(lst->element)
+					free(lst->element);
 			}
-			return 1;		
+			return;		
 		}
 		prev = lst;
 		lst = lst->next;
 	}
+	return;
+}
 
+
+void* find_hash(const void* self, const void* key){
+	size_t key_size = 1;
+	size_t tg = (((hash*)(((storage_t*)self)->manager))->HashFunc)(key, &key_size, ((storage_t*)self)->size);
+	key_size++;
+	hash* var = ((storage_t*)self)->manager;
+
+	list* lst = &(var->table[tg]);
+	while(lst->next != NULL){
+		if(!strncmp((char*)key, (char*)lst->key, key_size)){
+			return lst->element;		
+		}
+		lst = lst->next;
+	}
+	return NULL;
+}
+
+int clear_hash(void* self){
+	hash* var = (hash*)((storage_t*)self)->manager;
+	size_t i = 0;
+	size_t size = ((storage_t*)self)->size;
+	list* tmp = NULL;
+	list* temp = NULL;
+	for(i = 0; i < size; i++){
+		tmp = &(var->table[i]);
+		if(tmp->element){
+			free(tmp->element);
+			tmp->element = NULL;
+		}
+		if(tmp->key){
+			free(tmp->key);
+			tmp->key = NULL;
+		}
+		while(tmp->next != NULL){
+			temp = tmp->next;
+			tmp->next = tmp->next->next;	
+			if(temp->element){
+				free(temp->element);
+				temp->element = NULL;
+			}
+			if(temp->key){
+				free(temp->key);
+				temp->key = NULL;
+			}
+			free(temp);
+		}		
+	}
+	
+	return 0;
+}
+
+int delete_hash(void* self){
+	clear_hash(self);
+	((storage_t*)self)->add = NULL;
+	((storage_t*)self)->drop = NULL;
+	((storage_t*)self)->find = NULL;
+	((storage_t*)self)->clear = NULL;
+	((storage_t*)self)->delete = NULL;
+	if(((hash*)(((storage_t*)self)->manager))->table)
+		free(((hash*)(((storage_t*)self)->manager))->table);
+	if(((storage_t*)self)->manager)
+		free(((hash*)((storage_t*)self)->manager));
+	if((storage_t*)self)
+		free((storage_t*)self);
 	return 0;
 }
 
 
-extern void* find_hash(void* key, sequence* _var){
-	unsigned int key_size;
-	unsigned int tg = HashFunc(key, &key_size);
-	key_size++;
-	hash* var = _var->who;
-
-	list* lst = &(var->table[tg]);
-
-	while(lst->next != NULL){
-		if(!strncmp((char*)key, (char*)lst->key, key_size)){
-			return lst->data;		
-		}
-		lst = lst->next;
+storage_t* init_hash(size_t blsize, size_t hash_table_size, void* hashFunc){
+	storage_t* tmp = (storage_t*)malloc(sizeof(storage_t));
+	tmp->add = add_hash;
+	tmp->drop = drop_hash;
+	tmp->find = find_hash;
+	tmp->clear = clear_hash;
+	tmp->delete = delete_hash;
+	tmp->manager = (void*)malloc(sizeof(hash));
+	tmp->size = hash_table_size;
+	((hash*)(tmp->manager))->blsize = blsize;
+	((hash*)(tmp->manager))->table = (list*)malloc(hash_table_size * sizeof(list));
+	((hash*)(tmp->manager))->HashFunc = hashFunc;
+	size_t i;	
+	for(i = 0; i < hash_table_size; i++){
+		(((hash*)(tmp->manager))->table[i]).next = NULL;
+		(((hash*)(tmp->manager))->table[i]).element = NULL;
+		(((hash*)(tmp->manager))->table[i]).key = NULL;
 	}
-
-	return NULL;
-}
-
-sequence init_hash(size_t blsize){
-	sequence tmp;
-	tmp.insert = (&insert_hash);
-	tmp.delete = (&delete_hash);
-	tmp.find = (&find_hash);
-	tmp.who = (void*)malloc(sizeof(hash));
-	((hash*)(tmp.who))->blsize = blsize;
-	((hash*)(tmp.who))->table = malloc(1024 * sizeof(list*));
-
+		
 	return tmp;
 }
 
--- storage.h
+++ storage.h
@@ -6,18 +6,18 @@
 An interface for different approaches of data storage (hash table, black-red tree)
 */
 typedef struct storage_t{
-    void* manager; /**< The data structure responsible for storing and operating the data. */
-
-    size_t size;
-    
-    int   (*add)  (void* self, const void* element);
-    void  (*drop) (void* self, const void* element);
-    void* (*find) (const void* self, const void* element);
-    int   (*clear)(void* self, const void* element)
-    int   (*delete)(void* self);
+	int (*add)  (void* self, const void* key, const void* element);
+	void (*drop) (void* self, const void* key);
+	void* (*find) (const void* self, const void* key);
+	int (*clear)(void* self);
+	int (*delete)(void* self);
 } storage_t;
 
+
 storage_t* RBTree_ctor(size_t element_size);
-storage_t* init_hash();
+storage_t* init_hash(size_t element_size, size_t table_hash_size, void* hash_function);
+
+size_t HashAllData(void* Data);
+
 
 #endif // STORAGE_H
